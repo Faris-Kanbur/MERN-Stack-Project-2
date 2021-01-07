ed550f8b207ea2592a633c8ce0c16e2ba0cd695c
faris <faris38kanbur@gmail.com>
2021-01-07 12:27:42 +0100

    add db and router

---
+++ app.js
@@ -0,0 +1,19 @@
+ const express = require('express');
+ require('dotenv').config();
+ const dbConnect =require('./models/dbConnect')  // diger tarafta olusturulan baglanma fonksiyonunun calsmasi icin
+const router = require('./routers/router');
+
+
+
+ // server
+ const app = express();
+ const port = process.env.port || 5000;
+app.listen(port, () => {
+    console.log(`I am listening on port ${port}`);
+})
+
+//database connect
+dbConnect();
+
+app.use(express.json());
+app.use('/api', router);
